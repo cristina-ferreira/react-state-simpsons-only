4fd14ee9456b4a9d3e4f5de579bd8ff879a4065d
cristinaferreira <cristina.d.ferreira@sapo.pt>
2019-09-24 15:02:34 +0100

    Updates

---
+++ precache-manifest.09b8a702679f802704f231574ac30175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a658f1f32f1bb7c126a26365413cfe83",
+    "url": "/react-state-simpsons-only/index.html"
+  },
+  {
+    "revision": "d5c87d686d7ea15cd049",
+    "url": "/react-state-simpsons-only/static/css/main.af0a5a8e.chunk.css"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/react-state-simpsons-only/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "d5c87d686d7ea15cd049",
+    "url": "/react-state-simpsons-only/static/js/main.003cc1d5.chunk.js"
+  },
+  {
+    "revision": "2ca43cec06ce739d4a32",
+    "url": "/react-state-simpsons-only/static/js/runtime~main.96d8399a.js"
+  }
+]);
